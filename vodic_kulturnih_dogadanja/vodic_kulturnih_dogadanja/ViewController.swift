feebe92c232dcd284b92bd7c6006853ca3fcb0cf
Monika Tenšić <mtensic@foi.hr>
2017-10-26 23:03:03 +0200

    Kreirani su početni ekrani i veze među njima

---
+++ vodic_kulturnih_dogadanja/vodic_kulturnih_dogadanja/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  vodic_kulturnih_dogadanja
+//
+//  Created by MTLab on 26/10/2017.
+//  Copyright © 2017 foi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
