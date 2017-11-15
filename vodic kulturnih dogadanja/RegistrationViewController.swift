487a2ca0c44613708eafbc8db4ff919d408a6196
Petra Cvrljevic <pcvrljevic@ITs-MacBook-Pro.local>
2017-11-15 21:34:09 +0100

    Changes made on registration. Checking if fields are empty and showing alert or processing registration to the database if all fields are filled.

--- vodic kulturnih dogadanja/RegistrationViewController.swift
+++ vodic kulturnih dogadanja/RegistrationViewController.swift
@@ -20,6 +20,34 @@ class RegistrationViewController: UIViewController {
     @IBOutlet weak var surnameTextField: UITextField!
     
     @IBAction func registerButtonClick(_ sender: UIButton) {
+        /*if (userNameTextField.text == nil || (userNameTextField.text?.isEmpty)!) {
+            print("username")
+            return
+        }
+        if (passwordTextField.text == nil || (passwordTextField.text?.isEmpty)!) {
+            print("password")
+            return
+        }
+        if (emailTextField.text == nil || (emailTextField.text?.isEmpty)!) {
+            print("email")
+            return
+        }
+        if (nameTextField.text == nil || (nameTextField.text?.isEmpty)!) {
+            print("name")
+            return
+        }
+        if (surnameTextField.text == nil || (surnameTextField.text?.isEmpty)!) {
+            print("surname")
+            return
+        }*/
+        
+        if (userNameTextField.text == nil || (userNameTextField.text?.isEmpty)!) || (passwordTextField.text == nil || (passwordTextField.text?.isEmpty)!) || (emailTextField.text == nil || (emailTextField.text?.isEmpty)!) || (nameTextField.text == nil || (nameTextField.text?.isEmpty)!) || (surnameTextField.text == nil || (surnameTextField.text?.isEmpty)!) {
+            let emptyFieldsAlert = UIAlertController(title: "You have empty fields.", message: nil, preferredStyle: .alert)
+            emptyFieldsAlert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+            self.present(emptyFieldsAlert, animated: true, completion: nil)
+            return
+        }
+        
         let params: Parameters=[
             
             "username":userNameTextField.text!,
@@ -30,19 +58,11 @@ class RegistrationViewController: UIViewController {
             ]
         
         Alamofire.request(URL_USER_REGISTER, method: .post, parameters: params, encoding: JSONEncoding.default).responseString { (response) in
-            
-            print(response);
-            
-            let alertController = UIAlertController(title: "REGISTRATION SUCCESSFUL!", message: nil, preferredStyle: .alert);
-            
-            alertController.addAction(UIAlertAction(title: "OK", style: .default,handler: nil));
-            
-            
-            
+            print(response)
+            let alertController = UIAlertController(title: "REGISTRATION SUCCESSFUL!", message: nil, preferredStyle: .alert)
+            alertController.addAction(UIAlertAction(title: "OK", style: .default,handler: nil))
             self.present(alertController, animated: true, completion: nil)
-            
         }
-
     }
     
     
