69978560958710f421ded14e782cbaab8911ca6a
Petra Cvrljevic <pcvrljevic@ITs-MacBook-Pro.local>
2017-11-16 17:29:32 +0100

    Login fixed (shows alert when data is invalid). HTTPStatusCodes imported.

--- vodic kulturnih dogadanja/LoginViewController.swift
+++ vodic kulturnih dogadanja/LoginViewController.swift
@@ -2,14 +2,56 @@
 //  LoginViewController.swift
 //  vodic kulturnih dogadanja
 //
-//  Created by MTLab on 31/10/2017.
+//  Created by Petra Cvrljevic on 15/11/2017.
 //  Copyright © 2017 foi. All rights reserved.
 //
 
 import UIKit
+import Alamofire
+import HTTPStatusCodes
 
 class LoginViewController: UIViewController {
 
+    let URL_USER_LOGIN = "http://vodickulturnihdogadanja.1e29g6m.xip.io/userLogIn.php"
+    
+    let defaultValues = UserDefaults.standard
+    
+    @IBOutlet weak var userNameTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    @IBAction func loginButtonClick(_ sender: UIButton) {
+        //provjera
+        if (userNameTextField.text == nil || (userNameTextField.text?.isEmpty)!) || (passwordTextField.text == nil || (passwordTextField.text?.isEmpty)!) {
+            let emptyFieldsAlert = UIAlertController(title: "You have empty fields.", message: nil, preferredStyle: .alert)
+            emptyFieldsAlert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+            self.present(emptyFieldsAlert, animated: true, completion: nil)
+            return
+        }
+        
+        let params: Parameters=[
+            "username":userNameTextField.text!,
+            "password":passwordTextField.text!,
+        ]
+        
+        
+        Alamofire.request(URL_USER_LOGIN, method: .post, parameters: params, encoding: JSONEncoding.default).responseJSON {
+            response in
+            print(response)
+            let statusCode = (response.response?.statusCode)!
+            print(statusCode)
+            
+            if let result = response.result.value as? Dictionary<String, String> {
+                self.defaultValues.set(result["tokenId"]!, forKey: "tokenId")
+            }
+            else {
+                let alertController = UIAlertController(title: "LOGIN UNSUCCESSFUL", message: nil, preferredStyle: .alert)
+                alertController.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+                self.present(alertController, animated: true, completion: nil)
+                return
+            }
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
